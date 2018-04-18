791d7b47e4bc9253eaccff81a606c8bcaf7ae8ef
S4mmyB <samuel.bennetts@colorado.edu>
2018-04-18 14:37:39 -0600

    updated website format

--- website/src/App.js
+++ website/src/App.js
@@ -1,7 +1,7 @@
-import PropTypes from 'prop-types'
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
+import PropTypes from 'prop-types'
 import {
   Map,
   TileLayer,
@@ -10,109 +10,27 @@ import {
   PropTypes as MapPropTypes,
 } from 'react-leaflet'
 
-
-const MyPopupMarker = ({ children, position }) => (
-  <Marker position={position}>
-    <Popup>
-      <span>{children}</span>
-    </Popup>
-  </Marker>
-)
-MyPopupMarker.propTypes = {
-  children: MapPropTypes.children,
-  position: MapPropTypes.latlng,
-}
-
-const MyMarkersList = ({ markers }) => {
-  const items = markers.map(({ key, ...props }) => (
-    <MyPopupMarker key={key} {...props} />
-  ))
-  return <div style={{ display: 'none' }}>{items}</div>
-}
-MyMarkersList.propTypes = {
-  markers: PropTypes.array.isRequired,
-}
-
-class CustomComponent extends Component {
-  state = {
-    lat: 40.007581,
-    lng: -105.265942,
-    zoom: 14,
-  }
-
-  render() {
-    const center = [this.state.lat, this.state.lng]
-
-    const markers = [
-      { key: 'marker1', position: [51.5, -0.1], children: 'My first popup' },
-      { key: 'marker2', position: [51.51, -0.1], children: 'My second popup' },
-      { key: 'marker3', position: [51.49, -0.05], children: 'My third popup' },
-    ]
-    return (
-      <Map center={center} zoom={this.state.zoom}>
-        <TileLayer
-          attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <MyMarkersList markers={markers} />
-      </Map>
-    )
-  }
-}
-
-
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
+import Login from './components/login/userLogin';
+import Register from './components/register/register';
+import Homepage from './components/homepage/homepage';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">my<span style={{color:'#CFB584'}}>CU</span>pool</h1>
-          <a href="">
-		        <h1 className="App-locations">Locations</h1>
-		      </a>
-          <a href="">
-            <h1 className="App-register">Register</h1>
-          </a>
-          <a href="">
-            <h1 className="App-login">Login</h1>
-          </a>
-        </header>
-        <CustomComponent />
-        <div className="App-divider"></div>
-        <div className="App-mission">
-    		  <h1 className="App-mission-title">Our Mission</h1>
-    		  <p className="App-mission-message">Our goal for myCUpool is to create an easy and straight forward
-    		  method to facilitate CU student carpools that help save money, reduce environmental impacts, and
-    		  encourage the creation of new friendships. Carpooling benefits users by having multiple people use
-    		  one car instead of multiple, this saves money, reduces the volume of traffic, and helps the
-    		  environment. It is often difficult for people to find others who share similar interests, and thus
-    		  hard to find other people they would be willing to carpool with. By allowing our users to easily
-    		  connect with other people whether as a passenger or a driver themselves, we facilitate this process
-    		  of finding similar individuals as well as encouraging the benefits of carpooling. MyCUPool will
-    		  facilitate this process of finding others looking to go to locations/events, helping save gas,
-    		  reduce traffic, and help the environment.</p>
-    		</div>
-    		<div className="App-usage">
-    		  <h1 className="App-usage-title">How to use myCUpool</h1>
-    		  <p className="App-usage-message">The design of the website is made to be simple and streamlined you
-    		  can login or register at the top right of this page, and you will use your CU email to do so. Once
-    		  you have an account and filled out the information, you can view postings by selecting an area and
-    		  whether you want to see drivers/passengers/or both. You are also to make your own postings, as
-    		  either a driver looking for passengers, or a passenger looking for someone to be the driver. If you
-    		  are interested in contacting someone who made a posting you can use the button on their posting,
-    		  which will allow you to talk with about being a passenger or if you are willing to drive them. This
-    		  site is for CU students only, which means that we have limited users so we can have a closer sense
-    		  of community and accountability. Each user will have a rating connected to their profile, this will
-    		  show others whether they are a good driver/passenger or not, to prevent constantly tardy or
-    		  unenjoyable people from continuing to make others lives harder.</p>
-    		</div>
-        <div className="App-footer">
-          <h1 className="App-disclaimer-title">my<span style={{color:'#CFB584'}}>CU</span>pool</h1>
-          <p className= "App-disclaimer">is not responsible for any issues caused by the use of our application</p>
+      <Router>
+        <div>
+          <Header />
+
+            <Route exact path='/' component={Homepage} />
+            <Route exact path='/Login' component={Login} />
+            <Route exact path='/Register' component={Register} />
+
+
+          <Footer />
         </div>
-    </div>
+      </Router>
     );
   }
 }
